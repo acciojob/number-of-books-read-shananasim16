493d0b55e253fb1bfe4189f3e47f98f980ae92d9
SHANA SHABNAM NASIM <142229568+shananasim16@users.noreply.github.com>
2023-10-12 21:28:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
+// Define the function to count the number of books that have been read.
+function numberOfBooksRead(library) {
+    let count = 0;
+    
+    for (let i = 0; i < library.length; i++) {
+        if (library[i].readingStatus === true) {
+            count++;
+        }
+    }
+    
+    return count;
+}
+
+// Sample library object
 const library = [
-  {
-    author: "Bill Gates",
-    title: "The Road Ahead",
-    readingStatus: true,
-  },
-  {
-    author: "Steve Jobs",
-    title: "Walter Isaacson",
-    readingStatus: true,
-  },
-  {
-    author: "Suzanne Collins",
-    title: "Mockingjay: The Final Book of The Hunger Games",
-    readingStatus: false,
-  },
+    {
+        author: 'Author 1',
+        title: 'Book 1',
+        readingStatus: true,
+    },
+    {
+        author: 'Author 2',
+        title: 'Book 2',
+        readingStatus: false,
+    },
+    {
+        author: 'Author 3',
+        title: 'Book 3',
+        readingStatus: true,
+    },
 ];
 
-const numberOfBooksRead = () => {
-  // write your code here
-};
+// Call the function and display the result on the HTML page.
+const resultDiv = document.getElementById('result');
+const booksRead = numberOfBooksRead(library);
+resultDiv.textContent = `Number of books read: ${booksRead}`;
 
-// Do not change the code below
-
-alert(numberOfBooksRead());
